d6b13b86ce46a9a9b1644d050f42b13f7ef6b850
shiyawei <shiyawei@inspur.com>
2020-07-29 18:28:34 +0800

    06从尾到头打印列表

--- ReversePrint.cpp
+++ ReversePrint.cpp
@@ -1,4 +1,39 @@
 //
 // Created by yyc121 on 2020/7/29.
 //
+#include "ReversePrint.h"
+#include <stack>
+using namespace  std;
 
+vector<int> ReversePrint::reversePrint(ListNode *head, int flag) {
+    vector<int> res;
+    if(flag == 1){
+        //方法1：reverse反转法
+        while (head){
+            res.push_back(head->val);
+            head = head->next;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }else if(flag == 2) {
+        //方法2：入栈法
+        stack<int> s;
+        while (head) {
+            s.push(head->val);
+            head = head->next;
+        }
+        //出栈
+        while (!s.empty()) {
+            res.push_back(s.top());
+            s.pop();
+        }
+        return res;
+    }
+//    }else if(flag == 3){
+//        //方法3：递归
+//        if(head == nullptr)
+//            return res;
+//        reversePrint(head->next);
+//        res.push_back(head->val);
+//        return res;
+    }
--- ReversePrint.h
+++ ReversePrint.h
@@ -4,5 +4,17 @@
 
 #ifndef LEETCODE_C_REVERSEPRINT_H
 #define LEETCODE_C_REVERSEPRINT_H
+#include <vector>
+using namespace std;
 
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL){}
+};
+
+class ReversePrint{
+public:
+    vector<int> reversePrint(ListNode* head, int flag);
+};
 #endif //LEETCODE_C_REVERSEPRINT_H
